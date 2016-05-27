a29ce6cc3f8dfcdcca6c7019ca56c7f9405892c8
xzye <runningship@gmail.com>
2016-05-27 19:10:45 +0800

    customerlize ui

--- src/main/java/org/jasig/cas/authentication/SSOUsersAuthenticationHandler.java
+++ src/main/java/org/jasig/cas/authentication/SSOUsersAuthenticationHandler.java
@@ -1,4 +1,4 @@
-package com.jumore.sso.auth;
+package org.jasig.cas.authentication;
 
 import java.security.GeneralSecurityException;
 
@@ -8,6 +8,7 @@ import org.jasig.cas.authentication.HandlerResult;
 import org.jasig.cas.authentication.PreventedException;
 import org.jasig.cas.authentication.UsernamePasswordCredential;
 import org.jasig.cas.authentication.handler.support.AbstractUsernamePasswordAuthenticationHandler;
+import org.jasig.cas.authentication.principal.SimplePrincipal;
 import org.springframework.stereotype.Component;
 
 @Component("ssoUsersAuthenticationHandler")
@@ -22,7 +23,12 @@ public class SSOUsersAuthenticationHandler extends AbstractUsernamePasswordAuthe
 		System.out.println("You are login as "+ username+"/"+pwd);
 		logger.info("You are login as "+ username+"/"+pwd);
 		if("xzye".equals(username) && "xzye".equals(pwd)){
-			return createHandlerResult(credential, this.principalFactory.createPrincipal(username), null);
+			
+			//4.2.2做法
+//			return createHandlerResult(credential, this.principalFactory.createPrincipal(username), null);
+			
+			//4.0.0做法
+			return createHandlerResult(credential, new SimplePrincipal(username), null);
 		}
 		throw new FailedLoginException("用户名或密码不正确");
 		//TODO authenticate from database code here
